2233f8de50b73159366c160209fcae86cbe46b23
Javier Flores <javierfh1404@gmail.com>
2019-05-30 23:21:39 +0200

    sonar coverage

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,11 +1,13 @@
 public class Class1 {
 
     public int suma(int a, int b){
-        return a+b;
+        int result = a+b;
+        return result;
     }
 
     public int resta(int a, int b){
-        return a-b;
+        int result = a-b;
+        return result;
     }
 
 }
--- src/test/java/Class1Test.java
+++ src/test/java/Class1Test.java
@@ -6,6 +6,8 @@ public class Class1Test {
 
     @Test
     public void suma_positive() {
+        Class1 class1 = new Class1();
+        class1.suma(2,3);
     }
 
     @Test
