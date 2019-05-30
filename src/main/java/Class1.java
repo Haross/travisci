040306c76343979cc1e028711ba4c7e81677dcac
Javier Flores <javierfh1404@gmail.com>
2019-05-30 16:49:11 +0200

    first commit

---
+++ src/main/java/Class1.java
@@ -0,0 +1,11 @@
+public class Class1 {
+
+    public int suma(int a, int b){
+        return a+b;
+    }
+
+    public int resta(int a, int b){
+        return a-b;
+    }
+
+}
---
+++ src/test/java/Class1Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Class1Test {
+
+    @Test
+    public void suma() {
+    }
+
+    @Test
+    public void resta() {
+    }
+}
