6bae9ca1a40b68c5936ad927a6ef83c85199f33e
Javier Flores <javierfh1404@gmail.com>
2019-05-30 23:34:20 +0200

    test coverage

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,13 +1,11 @@
 public class Class1 {
 
     public int suma(int a, int b){
-        int result = a+b;
-        return result;
+        return a+b;
     }
 
     public int resta(int a, int b){
-        int result = a-b;
-        return result;
+        return  a-b;
     }
 
 }
